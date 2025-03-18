dfeeca826ecab3938eac22cda7b1a53909be594e
smitkapadiya07 <smitkapadiya07@gmail.com>
2025-03-18 13:39:21 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import Navigation from "./components/global/navigation.jsx";
+import HowItworks from "./pages/homepage/howItworks.jsx";
 
 function App() {
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <>
+            <Navigation/>
+            <HowItworks/>
+        </>
+    )
 }
 
 export default App
